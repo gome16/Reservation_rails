cfcf5eb58a5dc2e73478bb71abd0f99fe5be592d
YasuhiroHorigome <cork44@YasuhiroHorigomenoMacBook-Air.local>
2023-04-08 10:53:49 +0900

    予約機能実装

--- rails_reservation_app/app/controllers/reservations_controller.rb
+++ rails_reservation_app/app/controllers/reservations_controller.rb
@@ -3,25 +3,28 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.all
   end
 
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
   def new
   end
 
-  def create
-
-  end
   def confirm
-    @reservation = Reservation.new(@attr)
-    @reservation.user_id = current_user.id 
-    if @reservation.invalid?
-      render template: 'rooms/show'
-
-    end
+    @reservation = Reservation.new(reservation_params)
+    render template: "rooms/show" if @reservation.invalid?
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    render template: "rooms/show"  and return if params[:back] || !@reservation.save
+    redirect_to @reservation
+  end 
 
   private
 
-  def permit_params
-    @attr = params.require(:reservation).permit(:check_in, :check_out, :number, :room_id, :user_id)
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out, :number, :room_id, :user_id)
   end
   
 end
--- rails_reservation_app/db/schema.rb
+++ rails_reservation_app/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_03_033939) do
+ActiveRecord::Schema.define(version: 2023_04_08_012754) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
-    t.integer "check_in"
-    t.integer "check_out"
+    t.date "check_in"
+    t.date "check_out"
     t.integer "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
